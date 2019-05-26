0ad60d75af466721eeab328c3f3a926a58bdd831
xuyz <12510705@qq.com>
2019-05-26 23:02:47 +0800

    初始化

---
+++ src/main/java/com/xuyz/spring/Application.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Application
+ * Author:   xuyz
+ * Date:     2019/5/13 23:51
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.xuyz.spring;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author xuyz
+ * @create 2019/5/13
+ * @since 1.0.0
+ */
+public class Application {
+}
